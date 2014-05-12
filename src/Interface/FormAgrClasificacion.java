70a4ba6bad36d7aee674d60b48407fd49c23f044
Juan Ferreyra <juangabrielferreyra93@gmail.com>
2014-05-12 00:46:17 -0300

    agrego la interface con algunas de las cosas acordadas, faltan cosas

---
+++ src/Interface/FormAgrClasificacion.java
@@ -0,0 +1,50 @@
+package Interface;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class FormAgrClasificacion extends JFrame 
+{
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) 
+	{
+		EventQueue.invokeLater(new Runnable() 
+		{
+			public void run() 
+			{
+				try 
+				{
+					FormAgrClasificacion frame = new FormAgrClasificacion();
+					frame.setVisible(true);
+				} 
+				catch (Exception e) 
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FormAgrClasificacion() 
+	{
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 323, 454);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
--- src/Interface/MainFrame.java
+++ src/Interface/MainFrame.java
@@ -3,21 +3,47 @@ package Interface;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JButton;
+import javax.swing.UIManager;
 
-public class MainFrame {
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-	private JFrame frame;
+import javax.swing.JList;
+import javax.swing.JLabel;
+
+import java.awt.Font;
+
+import javax.swing.SwingConstants;
+
+import Negocio.Campeonato;
+
+public class MainFrame 
+{
+
+	private JFrame frmCampeonatoAutomovilistico;
+	private Campeonato _campeonato;
+	private FormAgrClasificacion _agrClasificacion;
+	private FormAgrFinalizacion _agrFinalizacion;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
+	public static void main(String[] args) 
+	{
+		EventQueue.invokeLater(new Runnable() 
+		{
+			public void run() 
+			{
+				try 
+				{
 					MainFrame window = new MainFrame();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
+					window.frmCampeonatoAutomovilistico.setVisible(true);
+				} catch (Exception e) 
+				{
 					e.printStackTrace();
 				}
 			}
@@ -27,18 +53,89 @@ public class MainFrame {
 	/**
 	 * Create the application.
 	 */
-	public MainFrame() {
+	public MainFrame() 
+	{
+		try 
+		{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} 
+		catch (Exception e) 
+		{
+			System.out.println("No encuentro en look and feel");
+			e.printStackTrace();
+		}
+		_campeonato = new Campeonato();
+		_agrClasificacion = new FormAgrClasificacion();
+		_agrFinalizacion = new FormAgrFinalizacion();
 		initialize();
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+	private void initialize() 
+	{
+		frmCampeonatoAutomovilistico = new JFrame();
+		frmCampeonatoAutomovilistico.setTitle("Campeonato Automovilistico");
+		frmCampeonatoAutomovilistico.setResizable(false);
+		frmCampeonatoAutomovilistico.setBounds(100, 100, 378, 334);
+		frmCampeonatoAutomovilistico.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmCampeonatoAutomovilistico.getContentPane().setLayout(null);
+		
+		JList listCarreras = new JList();
+		listCarreras.setBounds(276, 23, 96, 262);
+		frmCampeonatoAutomovilistico.getContentPane().add(listCarreras);
+		
+		JLabel lblCarreras = new JLabel("Carreras");
+		lblCarreras.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCarreras.setEnabled(false);
+		lblCarreras.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblCarreras.setBounds(276, -2, 86, 25);
+		frmCampeonatoAutomovilistico.getContentPane().add(lblCarreras);
+		
+		JButton btnFinalizacion = new JButton("Finalizacion");
+		btnFinalizacion.setBounds(147, 112, 119, 23);
+		frmCampeonatoAutomovilistico.getContentPane().add(btnFinalizacion);
+		btnFinalizacion.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				_agrFinalizacion.setVisible(true);
+				//TODO:deberia hacer que ingrese los resultados finales de la carrera
+			}
+		});
+		
+		JButton btnClasificacion = new JButton("Clasificacion");
+		btnClasificacion.setBounds(10, 112, 124, 23);
+		frmCampeonatoAutomovilistico.getContentPane().add(btnClasificacion);
+		btnClasificacion.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				_agrClasificacion.setVisible(true);
+				//TODO:deberia hacer que ingrese los resultados y tiempos de clasificacion
+			}
+		});
+		
+		JMenuBar menuBar = new JMenuBar();
+		frmCampeonatoAutomovilistico.setJMenuBar(menuBar);
+		
+		JMenu mnIngresar = new JMenu("Ingresar");
+		menuBar.add(mnIngresar);
+		
+		JMenuItem mntmPiloto = new JMenuItem("Piloto");
+		mnIngresar.add(mntmPiloto);
+		
+		JMenuItem mntmEvento = new JMenuItem("Evento");
+		mnIngresar.add(mntmEvento);
+		
+		JMenu mnVer = new JMenu("Ver");
+		menuBar.add(mnVer);
+		
+		JMenuItem mntmTablaPuntuaciones = new JMenuItem("Tabla de puntuaciones");
+		mnVer.add(mntmTablaPuntuaciones);
+		
+		JMenuItem mntmRankingSobrepasos = new JMenuItem("Ranking por sobrepasos");
+		mnVer.add(mntmRankingSobrepasos);
 	}
-
 }
